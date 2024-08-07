90ce45469b42b4f12c58284c371ca84bdb616d22
Ayush H <jqghebri@gmail.com>
2024-08-08 00:57:45 +0530

    creation

---
+++ Project-4/scripts.js
@@ -0,0 +1,8 @@
+const randomNumber = (parseInt(Math.random()*100 + 1));
+
+const submit  = document.querySelector('#Subt')
+const userInput  = document.querySelector('#guessField')
+const guessSlot  = document.querySelector('.guesses')
+const remaining  = document.querySelector('.lastResult')
+
+const lowOrHi  = document.querySelector('.loeOrHi')
