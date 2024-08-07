8b8ec68c4b7abcc8af2d62c4af78dbf30b653657
Ayush H <jqghebri@gmail.com>
2024-08-08 00:08:14 +0530

    creation

--- Project-2/script.js
+++ Project-2/script.js
@@ -13,8 +13,13 @@ form.addEventListener('submit', function(e){
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = "Please enter valid height."
     }
-    if(weight === '' || weight < 0 || isNaN(weight)){
+    else if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = "Please enter valid weight."
+    }
+    else{
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2);
+
+        results.innerHTML = `<span>Your BMI is ${bmi}</span?`;
     }
 
 })
